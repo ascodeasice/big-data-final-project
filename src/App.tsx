976f056cff15e7dab36172bd814a7dad2fc435be
ascodeasice <ascodeasice@gmail.com>
2025-05-19 16:17:16 +0800

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
 import Game from "./pages/Game";
+import { ChakraProvider, defaultSystem, defineConfig } from "@chakra-ui/react";
+import { ThemeProvider } from "@emotion/react";
+
+const config = defineConfig({
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+});
 
 const App = () => (
-  <>
-    <Switch>
-      <Route path="/">
-        <Home />
-      </Route>
-      <Route path="/game">
-        <Game />
-      </Route>
-      {/* Default route in a switch */}
-      <Route>404: No such page!</Route>
-    </Switch>
-  </>
+  <ChakraProvider value={defaultSystem}>
+    <ThemeProvider theme={config}>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/game">
+          <Game />
+        </Route>
+        {/* Default route in a switch */}
+        <Route>404: No such page!</Route>
+      </Switch>
+    </ThemeProvider>
+  </ChakraProvider>
 );
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import { Button, Center, Heading, Stack } from "@chakra-ui/react";
+import { Link } from "wouter";
+
 const Home = () => {
-  return <div>Home</div>;
+  // TODO: add more decoration
+  return (
+    <Center minH="100vh">
+      <Stack spaceY={6} align={"center"}>
+        <Heading size="4xl">韭菜傳奇</Heading>
+        <Link to="/game">
+          <Button colorPalette={"teal"}>開始遊戲</Button>
+        </Link>
+      </Stack>
+    </Center>
+  );
 };
 
 export default Home;
